d29e4895250537ec98fb2dfa2de5f32807ad30e4
nombre-usuario <email>
2025-04-07 14:52:05 -0600

    función multiplicar

--- diego.py
+++ diego.py
@@ -1,9 +1,6 @@
 print('Este archivo contiene una función de multiplicación')
 
-for i in range(10):
-    print(i)
-
 def multiplicacion(a, b):
     return a * b
 
-print(multiplicacion(6, 7))
+print(multiplicacion(3, 20))
